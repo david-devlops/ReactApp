7a00afcbe48ffa2cef3a0424371dbaf2b083a0da
David Hall <david@davidhalldevelops.com>
2021-08-17 19:51:20 -0400

    first external component--added Components

---
+++ src/Components/MyBlurb.js
@@ -0,0 +1,20 @@
+import React from "react";
+import reactDom from "react-dom";
+
+function MyBlurb() {
+    return (
+    <div>
+        <h1>David Hall</h1>
+        <p>
+            An aspiring React Developer, currently residing in the north Georgia hills. David has dedicated himself to the cause of creating a more accesible, kinder web.
+        </p>
+        <h3>Top Vacation Spots</h3>
+        <ol>
+            <li>Alaska</li>
+            <li>Train-hop through Pacific Northwest</li>
+            <li>Mobile, Alabama</li>
+        </ol>
+    </div>);
+}
+
+export default MyBlurb;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyBlurb from './Components/MyBlurb.js';
+
+//Initial React practice statement
+//ReactDOM.render(<h1>Hello World!</h1>,document.getElementById('root'));
+
+//UL practice segment - REACT TUTORIAL
+/*
+ReactDOM.render((<div>
+    <ul>
+        <li>Anything</li>
+        <li>you</li>
+        <li>want</li>
+    </ul>
+    </div>),document.getElementById('root'));
+*/
+//Functional components segment - REACT TUTORIAL
+/*
+function MyApp() {
+    return (
+    <ul>
+        <li>Anything</li>
+        <li>you</li>
+        <li>want</li>
+    </ul>
+    )
+}
+
+ReactDOM.render(<MyApp />,document.getElementById('root'));
+*/
+
+//Functional components segment -- part 2 -- REACT TUTORIAL
+
+
+ReactDOM.render(<MyBlurb />,document.getElementById('root'));
